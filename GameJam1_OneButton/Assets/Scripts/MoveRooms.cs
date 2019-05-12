6ffdd4fea573a7c27a17aee49e05506996bdbdf3
Atlas <AtlasTheTitan77@gmail.com>
2019-05-12 13:27:51 +0200

    Not Finished Yet

---
+++ GameJam1_OneButton/Assets/Scripts/MoveRooms.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRooms : MonoBehaviour
+{
+    public GameObject[] roomPrefabs;
+    public GameObject currentRoom;
+
+    public void MoveRoomsBack(){
+        GameObject randomRoom = roomPrefabs[Random.RandomRange(0,roomPrefabs.Length)];
+        randomRoom = Instantiate(randomRoom, new Vector3(0,0,20), Quaternion.identity);
+        randomRoom.transform.SetParent(this.transform);
+
+        randomRoom.GetComponent<Room>().MoveRoom(0.5f);
+        currentRoom.GetComponent<Room>().MoveRoom(0.5f);
+        currentRoom = randomRoom;
+    }
+}
---
+++ GameJam1_OneButton/Assets/Scripts/Room.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Transform backwall;
+    public LayerMask playerLayer;
+
+    private bool going = false;
+    private float t = 0;
+
+    private float startPosRoom;
+    private float startPosBackwall;
+    private float speed;
+    private bool hasPlayer;
+
+    public void MoveRoom(float _speed){
+        startPosRoom = transform.position.z;
+        startPosBackwall = backwall.position.y;
+        t = 0;
+        speed = _speed;
+        going = true;
+        hasPlayer = Physics.CheckBox(transform.position, new Vector3(10,8,20), Quaternion.identity, playerLayer);
+    }
+
+    public void Update(){
+        if(going){
+            if(hasPlayer){
+                backwall.position = new Vector3(backwall.position.x, Mathf.Lerp(startPosBackwall,10,t) ,backwall.position.z);
+            }
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(startPosRoom, startPosRoom-20, t));
+            t += speed * Time.deltaTime;
+            if(t >= 1.0f){
+                backwall.position = new Vector3(backwall.position.x, startPosBackwall ,backwall.position.z);
+                going = false;
+            }
+        }
+
+        if(transform.position.z <= -19){
+            going = false;
+            Destroy(this.gameObject);
+        }
+    }
+}
